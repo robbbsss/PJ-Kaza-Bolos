4c76d776aec11110fbd8e576e7b94d254455d766
Lucas Gomes <lucasgomes_tl@hotmail.com>
2022-04-03 02:39:28 -0400

    Adiciona background blur

--- pages/NewUser/Modal/index.js
+++ pages/NewUser/Modal/index.js
@@ -2,26 +2,30 @@ import React  from 'react';
 import { Alert, Modal, Text, Pressable, View } from 'react-native';
 import styles from './styles'
 
+const style = styles()
 const termsModal = ({modalVisible, setModalVisible}) => (
+  <>
       <Modal
-        animationType="slide"
+        animationType="fade"
         transparent
         visible={modalVisible}
         onRequestClose={() => {
           Alert.alert('Modal has been closed.');
           setModalVisible(!modalVisible);
         }}>
-        <View style={styles.centeredView}>
-          <View style={styles.modalView}>
-            <Text style={styles.modalText}>Hello World!</Text>
+        <View style={style.centeredView}>
+          <View style={style.modalView}>
+            <Text style={style.modalText}>Hello World!</Text>
             <Pressable
-              style={[styles.button, styles.buttonClose]}
+              style={[style.button, style.buttonClose]}
               onPress={() => setModalVisible(!modalVisible)}>
-              <Text style={styles.textStyle}>Ok, estou ciente</Text>
+              <Text style={style.textStyle}>Ok, estou ciente</Text>
             </Pressable>
           </View>
         </View>
       </Modal>
+      <View style={styles(modalVisible).blur} visible={modalVisible}/>
+      </>
   );
 
 
--- pages/NewUser/Modal/styles.js
+++ pages/NewUser/Modal/styles.js
@@ -1,11 +1,22 @@
 import {StyleSheet} from 'react-native'
 
-const styles = StyleSheet.create({
+const styles = (modalVisible) => StyleSheet.create({
   centeredView: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
     marginTop: 22,
+  },
+  blur: {
+    position: 'absolute',
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 0,
+    backgroundColor: '#000',
+    opacity: `${modalVisible ? '0.5' : '0'}`,
+    visibility: `${modalVisible ? 'visible' : 'hidden'}`,
+    transition: 'opacity 0.5s'
   },
   modalView: {
     margin: 20,
